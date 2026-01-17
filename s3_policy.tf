a9f01b6dd2e43f1b9462174892434c2a2fa3042a
Soniya Kothari <soniyakothari1511@gmail.com>
2026-01-17 11:58:43 +0530

    Add S3 bucket policy

---
+++ s3_policy.tf
@@ -0,0 +1,17 @@
+resource "aws_s3_bucket_policy" "teamflow_bucket_policy" {
+  bucket = aws_s3_bucket.teamflow_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = "*"
+        Action = [
+          "s3:GetObject"
+        ]
+        Resource = "${aws_s3_bucket.teamflow_bucket.arn}/*"
+      }
+    ]
+  })
+}
