7eb353061a751738724b65f65566995fd108306b
Soniya Kothari <soniyakothari1511@gmail.com>
2026-01-19 12:31:38 +0530

    Add SQS queues with redrive policy

---
+++ sqs.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_sqs_queue" "dlq" {
+  name = "simple-dlq"
+}
+
+resource "aws_sqs_queue" "main" {
+  name = "simple-main-queue"
+
+  redrive_policy = jsonencode({
+    deadLetterTargetArn = aws_sqs_queue.dlq.arn
+    maxReceiveCount     = 3
+  })
+}
