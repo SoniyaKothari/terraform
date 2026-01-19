090da5cddb93df5e9eabd07e2f6bcd3edc73b8be
Soniya Kothari <soniyakothari1511@gmail.com>
2026-01-19 12:35:27 +0530

    Updated sqs.tf
    Added message retention and tags to SQS queues.

--- sqs.tf
+++ sqs.tf
@@ -12,14 +12,27 @@ provider "aws" {
 }
 
 resource "aws_sqs_queue" "dlq" {
-  name = "simple-dlq"
+  name                      = "simple-dlq"
+  message_retention_seconds = 1209600 # 14 days
+
+  tags = {
+    Purpose = "dead-letter-queue"
+    Env     = "dev"
+  }
 }
 
 resource "aws_sqs_queue" "main" {
-  name = "simple-main-queue"
+  name                      = "simple-main-queue"
+  visibility_timeout_seconds = 30
+  message_retention_seconds  = 345600 # 4 days
 
   redrive_policy = jsonencode({
     deadLetterTargetArn = aws_sqs_queue.dlq.arn
     maxReceiveCount     = 3
   })
+
+  tags = {
+    Purpose = "main-processing-queue"
+    Env     = "dev"
+  }
 }
