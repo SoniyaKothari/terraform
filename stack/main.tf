88091bb3224911a565540364680e0fdcf26d79e7
Soniya Kothari <soniyakothari1511@gmail.com>
2026-02-01 20:36:26 +0530

    Create VPC, public subnet, and security group in Terraform
    Added resources for VPC, public subnet, and security group with appropriate configurations and tags.

---
+++ stack/main.tf
@@ -0,0 +1,44 @@
+resource "aws_vpc" "this" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name        = "${var.project_name}-${var.environment}-vpc"
+    Environment = var.environment
+  }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name        = "${var.project_name}-${var.environment}-public-subnet"
+    Environment = var.environment
+  }
+}
+
+resource "aws_security_group" "default" {
+  name   = "${var.project_name}-${var.environment}-sg"
+  vpc_id = aws_vpc.this.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Environment = var.environment
+  }
+}
